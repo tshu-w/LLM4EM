617ac5f5d1629f4f41aebc3fe24c04b994b0e0d1
Tianshu Wang <wang@tianshu.me>
2024-06-12 03:25:36 +0000

    Update selecting_pos.py

--- src/selecting_pos.py
+++ src/selecting_pos.py
@@ -1,68 +1,19 @@
 import re
-from functools import wraps
 from pathlib import Path
 
 import pandas as pd
 from diskcache import Cache
 from jinja2 import Template
-from openai import OpenAI
 from rich import print
 from sklearn.metrics import classification_report
-from tenacity import retry, stop_after_attempt, wait_exponential
 from tqdm.contrib.concurrent import thread_map
 
-cache = Cache("results/diskcache/selecting")
-
-# From https://openai.com/pricing#language-models at 2024.01.01
-MODEL_COST_PER_1K_TOKENS = {
-    "gpt-3.5-turbo": {"prompt": 0.0015, "completion": 0.0020},
-    "gpt-3.5-turbo-0301": {"prompt": 0.0015, "completion": 0.0020},
-    "gpt-3.5-turbo-0613": {"prompt": 0.0015, "completion": 0.0020},
-    "gpt-3.5-turbo-1106": {"prompt": 0.0010, "completion": 0.0020},
-    "gpt-4": {"prompt": 0.03, "completion": 0.06},
-}
-LLM = "gpt-3.5-turbo"
-# Global variable to accumulate cost
-ACCUMULATED_COST = 0
+from src.utils import APICostCalculator, openai_chat_complete
 
 
-def api_cost_decorator(model_name):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            response = func(*args, **kwargs)
-            cost = (
-                MODEL_COST_PER_1K_TOKENS[model_name]["prompt"]
-                * response.usage.prompt_tokens
-                + MODEL_COST_PER_1K_TOKENS[model_name]["completion"]
-                * response.usage.completion_tokens
-            ) / 1000
-            global ACCUMULATED_COST
-            ACCUMULATED_COST += cost
-            return response
-
-        return wrapper
-
-    return decorator
-
-
-@api_cost_decorator(model_name=LLM)
-@cache.memoize(name="__main__.chat_complete")
-@retry(stop=stop_after_attempt(10), wait=wait_exponential(multiplier=1, max=10))
-def chat_complete(
-    messages,
-    model=LLM,
-    client=OpenAI(),
-    **kwargs,
-):
-    return client.chat.completions.create(messages=messages, model=model, **kwargs)
-
-
-def select(
-    instance,
-    model=LLM,
-    template=Template(
-        """Select a record from the following candidates that refers to the same real-world entity as the given record. Answer only with the corresponding record number surrounded by "[]" or "[0]" if there is none.
+class Selecting:
+    template = Template(
+        """Select a record from the following candidates that refers to the same real-world entity as the given record. Answer with the corresponding record number surrounded by "[]" or "[0]" if there is none.
 
 Given entity record:
 {{ anchor }}
@@ -70,39 +21,64 @@ Given entity record:
 Candidate records:{% for candidate in candidates %}
 [{{ loop.index }}] {{ candidate }}{% endfor %}
 """
-    ),
-) -> list[bool]:
-    response = chat_complete(
-        messages=[
-            {
-                "role": "user",
-                "content": template.render(
-                    anchor=instance["anchor"],
-                    candidates=instance["candidates"],
-                ),
-            }
-        ],
-        model=model,
-        logprobs=True,
-        seed=42,
-        temperature=0.0,
-        max_tokens=5,
     )
 
-    idx = re.search(r"\[(\d+)\]", response.choices[0].message.content.strip())
-    preds = [False] * len(instance["candidates"])
-    if idx:
-        idx = int(idx.group(1))
-        if 1 <= idx <= len(instance["candidates"]):
-            preds[idx - 1] = True
+    def __init__(
+        self,
+        model_name: str = "gpt-3.5-turbo-0613",
+        template: Template = template,
+    ):
+        self.model = model_name
+        self.template = template
 
-    return preds
+        self.api_cost_decorator = APICostCalculator(model_name=model_name)
+        cache = Cache(f"results/diskcache/selecting_{model_name}")
+        self.chat_complete = self.api_cost_decorator(
+            cache.memoize(name="chat_complete")(openai_chat_complete)
+        )
+
+    def __call__(self, instance) -> list[bool]:
+        response = self.chat_complete(
+            messages=[
+                {
+                    "role": "user",
+                    "content": self.template.render(
+                        anchor=instance["anchor"],
+                        candidates=instance["candidates"],
+                    ),
+                }
+            ],
+            model=self.model,
+            seed=42,
+            temperature=0.0,
+            logprobs=self.model.startswith("gpt"),
+            top_logprobs=3 if self.model.startswith("gpt") else None,
+            max_tokens=3,
+        )
+
+        idx = re.search(r"\[(\d+)\]", response.choices[0].message.content.strip())
+        preds = [False] * len(instance["candidates"])
+        if idx:
+            idx = int(idx.group(1))
+            if 1 <= idx <= len(instance["candidates"]):
+                preds[idx - 1] = True
+
+        return preds
+
+    @property
+    def cost(self):
+        return self.api_cost_decorator.cost
+
+    @cost.setter
+    def cost(self, value: int):
+        self.api_cost_decorator.cost = value
 
 
 if __name__ == "__main__":
-    for pos in range(0, 10):
+    for pos in range(0, 10, 2):
         results = {}
         dataset_files = sorted(Path("data/llm4em").glob("*.csv"))
+        selector = Selecting()
         for file in dataset_files:
             dataset = file.stem
             print(f"[bold magenta]{dataset}[/bold magenta]")
@@ -135,7 +111,7 @@ if __name__ == "__main__":
                 )
 
             preds_lst = thread_map(
-                select,
+                selector,
                 instances,
                 max_workers=16,
             )
@@ -148,16 +124,12 @@ if __name__ == "__main__":
             results[dataset].pop("support")
             for k, v in results[dataset].items():
                 results[dataset][k] = v * 100
-            results[dataset]["cost"] = ACCUMULATED_COST
-            ACCUMULATED_COST = 0
 
         results["mean"] = {
             "precision": sum(v["precision"] for v in results.values()) / len(results),
             "recall": sum(v["recall"] for v in results.values()) / len(results),
             "f1-score": sum(v["f1-score"] for v in results.values()) / len(results),
-            "cost": sum(v["cost"] for v in results.values()) / len(results),
         }
         df = pd.DataFrame.from_dict(results, orient="index")
-        print(f"[bold red]{pos}[/bold red]")
         print(df)
         print(df.to_csv(float_format="%.2f", index=False))
