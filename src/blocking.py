bf2022914d0e4f51ffabea3535cc8cd0bfb5f943
Tianshu Wang <wang@tianshu.me>
2024-09-18 06:30:15 +0000

    Update blocking for more baselines

--- src/blocking.py
+++ src/blocking.py
@@ -82,6 +82,13 @@ if __name__ == "__main__":
         matches.columns = ["id_left", "id_right"]
         match_set = set(matches.itertuples(index=False, name=None))
 
+        # # HACK: We foget to update the retrieval index with code update, so we build the index with old data
+        # old_dfs = [pd.read_csv(path / f, index_col="id", sep=sep) for f in files]
+        # for df in old_dfs:
+        #     df.fillna("", inplace=True)
+
+        # _ = blocking(dataset, old_dfs, match_set)
+
         candidates = blocking(dataset, dfs, match_set)
         candidates = pd.DataFrame(candidates, columns=["id_left", "id_right"])
 
@@ -154,26 +161,25 @@ if __name__ == "__main__":
         )
         candidates_sample.to_csv(Path("data/llm4em") / f"{dataset}.csv", index=False)
 
+        # Prepare training data for Baselines
         candidates_remains = shuffle(
             candidates_remains.reset_index(drop=True), random_state=42
         )
-        candidates_remains.to_csv(
-            Path("data/llm4em/remains") / f"{dataset}.csv", index=False
-        )
-
-        # Prepare training data for Sudowoodo
         for key in ["record_left", "record_right"]:
             candidates_sample[key] = candidates_sample[key].str.replace("\t", " ")
             candidates_remains[key] = candidates_remains[key].str.replace("\t", " ")
-        Path(f"data/llm4em/sudowoodo/{dataset}").mkdir(parents=True, exist_ok=True)
+
         candidates_remains["label"] = candidates_remains["label"].astype(int)
+        candidates_sample["label"] = candidates_sample["label"].astype(int)
+
+        # Sudowoodo
+        Path(f"data/llm4em/sudowoodo/{dataset}").mkdir(parents=True, exist_ok=True)
         candidates_remains[["record_left", "record_right", "label"]][:5000].to_csv(
             Path(f"data/llm4em/sudowoodo/{dataset}") / "train.txt",
             index=False,
             sep="\t",
             header=False,
         )
-        candidates_sample["label"] = candidates_sample["label"].astype(int)
         candidates_sample[["record_left", "record_right", "label"]].to_csv(
             Path(f"data/llm4em/sudowoodo/{dataset}") / "test.txt",
             index=False,
@@ -184,6 +190,36 @@ if __name__ == "__main__":
         allpair = pd.concat([candidates_remains[:5000], candidates_sample])
         allpair[["record_left", "record_right"]][: topK * len(dfs[0])].to_csv(
             Path(f"data/llm4em/sudowoodo/{dataset}") / "train_no_label.txt",
+            index=False,
+            sep="\t",
+            header=False,
+        )
+
+        # Ditto, HierGAT
+        train_df = pd.concat(
+            [
+                candidates_remains[candidates_remains["label"] == 1][:500],
+                candidates_remains[candidates_remains["label"] == 0][:4500],
+            ]
+        )[["record_left", "record_right", "label"]]
+        Path(f"data/llm4em/sft/{dataset}").mkdir(parents=True, exist_ok=True)
+        from sklearn.model_selection import train_test_split
+
+        train, valid = train_test_split(train_df, test_size=0.2)
+        train.to_csv(
+            Path(f"data/llm4em/sft/{dataset}") / "train.txt",
+            index=False,
+            sep="\t",
+            header=False,
+        )
+        valid.to_csv(
+            Path(f"data/llm4em/sft/{dataset}") / "valid.txt",
+            index=False,
+            sep="\t",
+            header=False,
+        )
+        candidates_sample[["record_left", "record_right", "label"]].to_csv(
+            Path(f"data/llm4em/sft/{dataset}") / "test.txt",
             index=False,
             sep="\t",
             header=False,
